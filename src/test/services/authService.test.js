b3ac97c1ec7cb1a742bd4ad52af89d44871cea98
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-09-15 12:25:40 -0300

    observacao triple A

--- src/test/services/authService.test.js
+++ src/test/services/authService.test.js
@@ -5,14 +5,14 @@ const authService = new AuthService();
 
 describe("Testando a authService.cadastrarUsuario", () => {
   it("O usuário deve possuir um nome, email e senha", async () => {
-    //arrage
+    //arrange -> prepara o teste com os parâmetros
     const usuarioMock = {
       nome: "Rafael",
       email: "r@r.com",
     };
-    //act
+    //act -> aciona método que retornará as informações
     const usuarioSalvo = authService.cadastrarUsuario(usuarioMock);
-    //assert
+    //assert -> faz a validação das informações retornadas
     await expect(usuarioSalvo).rejects.toThrowError(
       "A senha de usuário é obrigatória!"
     );
