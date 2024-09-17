82f7e2700a78080c64494ac53a873359e155d778
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-09-17 19:40:00 -0300

    dbConfig

--- src/test/db/dbConfig.test.js
+++ src/test/db/dbConfig.test.js
@@ -1,1 +1,20 @@
-import db from "../../db/dbConfig.js"
+import { describe, expect } from "@jest/globals";
+import db from "../../db/dbConfig.js";
+
+describe("Testando configDB", () => {
+  it("Teste de conexão com o banco de dados", async () => {
+    const autorMock = {
+      nome: "Filipe",
+      nacionalidade: "Brasileiro",
+      created_at: new Date().toISOString(),
+      update_at: new Date().toISOString(),
+    };
+
+    const autorSalvo = await db("autores")
+      .insert(autorMock)
+      .then(retorno, () => db("autores").where("id", retorno[0]))
+      .then((autorSelecionado) => autorSelecionado[0]);
+
+    expect(autorSalvo.nome).toBe(autorMock.nome);
+  });
+});
